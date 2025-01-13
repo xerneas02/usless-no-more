c56fe3df6f470eb3f82969064c997a6ec49f922d
xerneas02 <mathis.roubille@gmail.com>
2025-01-13 23:56:41 +0100

    Add guardian shard and trident craft

--- src/main/java/net/xerneas/uslessnomore/UselessNoMore.java
+++ src/main/java/net/xerneas/uslessnomore/UselessNoMore.java
@@ -2,15 +2,47 @@ package net.xerneas.uslessnomore;
 
 import net.fabricmc.api.ModInitializer;
 
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import net.xerneas.uslessnomore.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UselessNoMore implements ModInitializer {
 	public static final String MOD_ID = "useless-no-more";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	private static final RegistryKey ELDER_GUARDIAN_TABLE_ID = EntityType.ELDER_GUARDIAN.getLootTableId();
+	private static final RegistryKey GUARDIAN_TABLE_ID = EntityType.GUARDIAN.getLootTableId();
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
+		// Add item to entities loot tables
+		LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+			if (source.isBuiltin() && ELDER_GUARDIAN_TABLE_ID.equals(key)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.rolls(UniformLootNumberProvider.create(0, 2))
+						.with(ItemEntry.builder(ModItems.ELDER_GUARDIAN_SHARD));
+
+				tableBuilder.pool(poolBuilder);
+			}
+		});
+
+		LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+			if (source.isBuiltin() && GUARDIAN_TABLE_ID.equals(key)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.rolls(UniformLootNumberProvider.create(0, 4))
+						.with(ItemEntry.builder(ModItems.GUARDIAN_SHARD));
+
+				tableBuilder.pool(poolBuilder);
+			}
+		});
 	}
 }
---
+++ src/main/java/net/xerneas/uslessnomore/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.xerneas.uslessnomore.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.xerneas.uslessnomore.UselessNoMore;
+
+public class ModItems {
+    public static final Item ELDER_GUARDIAN_SHARD = registerItem("elder_guardian_shard", new Item(new Item.Settings().rarity(Rarity.RARE)));
+    public static final Item GUARDIAN_SHARD = registerItem("guardian_shard", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(UselessNoMore.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        UselessNoMore.LOGGER.info("Registering Mod Items for " + UselessNoMore.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(ELDER_GUARDIAN_SHARD);
+            entries.add(GUARDIAN_SHARD);
+        });
+    }
+}
