1fcef8f1d4117584bb5acf5029d3ac9f7edb79c0
xerneas02 <mathis.roubille@gmail.com>
2025-01-13 21:58:23 +0100

    First commit

---
+++ src/main/java/net/xerneas/uslessnomore/UselessNoMore.java
@@ -0,0 +1,16 @@
+package net.xerneas.uslessnomore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UselessNoMore implements ModInitializer {
+	public static final String MOD_ID = "useless-no-more";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/xerneas/uslessnomore/UselessNoMoreClient.java
@@ -0,0 +1,10 @@
+package net.xerneas.uslessnomore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UselessNoMoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
